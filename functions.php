02b5d3d5eb4b92dd10c0691ebdb024deaf3764f5
LumBerisha-maker <lumberisha12@outlook.com>
2026-05-19 20:47:45 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,3 @@
+wp_enquene_style("style",get_stylesheet_url());
+
+<!-- wp_enquene_style("slider",get_template_diectory_url().'/css/slider.css',false,'1.1','all') -->;
---
+++ header.php
@@ -0,0 +1,17 @@
+<!docotype html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>DS Theme</title>
+</head>
+   <body>
+     <header>
+        <nav>
+            <ul>
+                <li>Contact us</li>
+                 <li>About us</li>
+                  <li>Register</li>
+</ul>
+</nav>
+</header>
+ 
